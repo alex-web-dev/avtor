9443e8da68eeb206dee0f4772a754fa0b2bd25ed
Alex <lesha.rybalko.1998@mail.ru>
2021-07-20 14:38:20 +0300

    Version 1.1.0

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ window.addEventListener('load', () => {
   }
 
 
-  /** Подгрузка изображений */
+//   /** Подгрузка изображений */
   const firstImagesLength = 6;
   const loadImagesLength = 3;
 
@@ -33,13 +33,30 @@ window.addEventListener('load', () => {
   });
 });
 
+
 const bannerBtn = document.querySelector('.banner__btn');
-bannerBtn.addEventListener('click', showCallback);
+const callbackPopup = document.querySelector('.callback-popup');
+bannerBtn.addEventListener('click', () => {
+  callbackPopup.classList.add('callback-popup_active');
 
-function showCallback() {
+});
 
-}
+const callbackPopupClose = callbackPopup.querySelector('.callback-popup__close');
+callbackPopupClose.addEventListener('click', () => {
+  callbackPopup.classList.remove('callback-popup_active');
+});
 
+callbackPopup.addEventListener('click', (e) => {
+  if (typeof e.target.className !== 'string') {
+    return;
+  }
+
+  if (e.target.className.includes('callback-popup_active')) {
+    callbackPopup.classList.remove('callback-popup_active');
+  }
+});
+
+/* Попап галереи */
 const galleryImages = document.querySelectorAll('.gallery__img');
 const popupImg = document.querySelector('.popup-img');
 const popupImgValue = document.querySelector('.popup-img__img');
@@ -80,3 +97,102 @@ popupPrev.addEventListener('click', () => {
   imgIndex--;
   popupImgValue.src = galleryImages[imgIndex - 1].src;
 });
+
+
+/* Бургер меню*/
+const menu = document.querySelector('.menu');
+const adaptiveValue = 992;
+const toggleMenu = menu.querySelector('.menu__toggle');
+const closeMenu = menu.querySelector('.menu__close');
+
+toggleMenu.addEventListener('click', () => {
+  menu.classList.toggle('menu_active')
+});
+
+closeMenu.addEventListener('click', () => {
+  menu.classList.remove('menu_active')
+});
+
+menu.addEventListener('click', (e) => {
+  if (window.innerWidth > adaptiveValue) {
+    return;
+  }
+
+  if (e.target.className.includes('header__menu')) {
+    menu.classList.remove('menu_active');
+  }
+});
+
+
+/* Слайдер видео */
+const videoList = document.querySelector('.videos__list');
+const videos = videoList.querySelectorAll('.videos__item');
+const videosDots = document.querySelectorAll('.videos__dot');
+videosDots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    let videoWidth;
+    if (window.innerWidth > 1400) {
+      videoWidth = 341;
+    } else if (window.innerWidth > 992) {
+      videoWidth = 205;
+    } else {
+      videoWidth = 0;
+    }
+
+    if (index === 0) {
+      videoList.style.transform = `translateX(${videoWidth}px)`;
+    } else {
+      videoList.style.transform = `translateX(${-videoWidth * (index - 1)}px)`;
+    }
+
+    const activeVideo = videoList.querySelector('.videos__item_active');
+    const activeDot = document.querySelector('.videos__dot_active');
+
+    activeVideo.classList.remove('videos__item_active');
+    activeDot.classList.remove('videos__dot_active');
+    
+    videos[index].classList.add('videos__item_active');
+    videosDots[index].classList.add('videos__dot_active');
+  });
+})
+
+/* Включение YouTube видео */
+const videoPlays = document.querySelectorAll('.videos__watch-btn');
+videoPlays.forEach(playBtn => {
+  playBtn.addEventListener('click', () => {
+    const videoItem = playBtn.closest('.videos__item');
+
+    if (!videoItem.querySelector('iframe')) {
+      const videoUrl = playBtn.dataset.url;
+      const videoFrame = getYoutubeFrame(videoUrl);
+  
+      videoItem.appendChild(videoFrame);
+    }
+  });
+})
+
+function getYoutubeFrame(src) {
+  const frame = document.createElement('iframe');
+
+  frame.setAttribute('src', `${src}?autoplay=1`);
+  frame.setAttribute('width', '100%');
+  frame.setAttribute('height', '100%');
+  frame.setAttribute('title', 'YouTube video player');
+  frame.setAttribute('autoplay', '');
+  frame.setAttribute('frameborder', '0');
+  frame.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
+  frame.setAttribute('allowfullscreen', '');
+
+  return frame;
+}
+
+
+const callbackForms = document.querySelectorAll('.callback-form');
+callbackForms.forEach(form => {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    console.log(form);
+    
+  });
+})
